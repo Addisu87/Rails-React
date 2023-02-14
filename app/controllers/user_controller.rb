9ea87ea8975bed68b1e52b74629ac54b6fd7a6e1
Addisu <addisuhaile87@gmail.com>
2023-02-14 15:31:50 +0300

    Update current_user controller

--- app/controllers/user_controller.rb
+++
@@ -1,7 +0,0 @@
-class UserController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    render json: user, status: :ok
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/user', to: 'user#index'
+  get '/current_user', to: 'current_user#index'
 
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
