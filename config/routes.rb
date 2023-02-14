d0145d065ac1a9747ec68c26f4a23f0c41deb809
Addisu <addisuhaile87@gmail.com>
2023-02-14 14:52:51 +0300

    Add devise and rack-cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Defines the root path route ("/")
   root 'pages#home'
 
   get '*path', to: 'pages#home', via: :all
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
 end
