6f9c887b794a5bd06d16ae1e3b3e8eed036b02a9
Addisu <addisuhaile87@gmail.com>
2023-02-14 14:58:18 +0300

    Update dispatch requests in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # Defines the root path route ("/")
   root 'pages#home'
 
   get '*path', to: 'pages#home', via: :all
-  
+
 end
