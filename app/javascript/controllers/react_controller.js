f4878b24a3600cb2e3bf3a86595a72cbd5bf0967
Addisu <addisuhaile87@gmail.com>
2023-02-15 14:51:20 +0300

    Install react-redux

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from '../components/App';
 
 // Connects to data-controller="react"
@@ -8,6 +9,10 @@ export default class extends Controller {
   connect() {
     const container = document.getElementById('root');
     const root = createRoot(container);
-    root.render(<App />);
+    root.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   }
 }
