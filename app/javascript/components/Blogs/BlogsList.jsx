78bd062a15728f1997c1c58ad9c455ddb7f49e1a
Addisu <addisuhaile87@gmail.com>
2023-02-15 17:00:44 +0300

    react-loader-spinner

--- app/javascript/components/Blogs/BlogsList.jsx
+++ app/javascript/components/Blogs/BlogsList.jsx
@@ -7,12 +7,28 @@ const BlogsList = () => {
   const blogs = useSelector(selectAllBlogs);
 
   const blogStatus = useSelector((state) => state.blogs.status);
+  const error = useSelector((state) => state.blogs.error);
 
   useEffect(() => {
     if (blogStatus === 'idle') {
       dispatch(fetchBlogs());
     }
   }, [blogStatus, dispatch]);
+
+  let content;
+
+  if (postStatus === 'loading') {
+    content = (
+      <RotatingLines
+        text="Loading..."
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    );
+  }
 
   return <div>BlogsList</div>;
 };
