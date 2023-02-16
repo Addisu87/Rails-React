9e3a0a1cfcd7786ee30f3b6bfa7b2b1d7bbce48e
Addisu <addisuhaile87@gmail.com>
2023-02-16 22:19:40 +0300

    Modify the sidebar icons

--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -26,51 +26,47 @@ const Sidebar = () => {
       <div className="w-full h-full bg-gray-200">
         <div className="flex flex-no-wrap">
           {/* Sidebar starts */}
-
-          <nav className="absolute h-screen z-40 lg:relative w-64 md:w-96 drop-shadow-md pb-4 bg-gray-100 hidden lg:block transition duration-150 ease-in-out">
-            <div
+          <nav
+            className={`absolute h-screen z-40 lg:relative w-64 md:w-96 drop-shadow-md pb-4 bg-gray-100 sm:hidden lg:block transition duration-150 ease-in-out ${
+              show
+                ? 'w-full h-full absolute z-40  transform  translate-x-full'
+                : 'w-full h-full absolute z-40  transform translate-x-0'
+            }`}
+          >
+            <button
+              type="button"
+              className="fixed z-40 cursor-pointer border-none left-8 right-8"
               onClick={() => setShow(!show)}
-              className="bg-gray-200 opacity-50 absolute h-full w-full sm:hidden"
             >
-              <button
-                type="button"
-                className="fixed z-40 cursor-pointer  border-none left-8 right-8"
+              {show ? (
+                <MdClose className="text-color-gray w-10 h-10" />
+              ) : (
+                <FaBars className="text-color-gray w-10 h-10" />
+              )}
+            </button>
+
+            <div className="bg-gray-200 opacity-50 absolute h-full w-full sm:hidden">
+              <ul
+                aria-orientation="vertical"
+                className={`py-6 ${show ? 'w-full' : 'hidden'}`}
               >
-                {show ? (
-                  <MdClose className="text-color-gray w-10 h-10" />
-                ) : (
-                  <FaBars className="text-color-gray w-10 h-10" />
-                )}
-              </button>
+                {SideLinks.map(({ title, url, icon }, i) => (
+                  <li
+                    key={i}
+                    className="pl-6 cursor-pointer text-slate-700 text-sm leading-3 tracking-normal pb-4 pt-5 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
+                  >
+                    <div className="flex items-center">
+                      <a href={url} className="w-6 h-6 md:w-8 md:h-8 ">
+                        <span className="text-gray-600 h-6 w-6 ">{icon}</span>
+                        <span className="ml-2 xl:text-base md:text-2xl text-base">
+                          {title}
+                        </span>
+                      </a>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
-
-            <div className="h-16 w-full flex items-center px-8">
-              <span>
-                <FaSearch />
-              </span>
-              <h1 className="uppercase">Sidebar</h1>
-            </div>
-
-            <ul
-              aria-orientation="vertical"
-              className={`py-6 ${show ? 'w-full' : 'hidden'}`}
-            >
-              {SideLinks.map(({ title, url, icon }, i) => (
-                <li
-                  key={i}
-                  className="pl-6 cursor-pointer text-slate-700 text-sm leading-3 tracking-normal pb-4 pt-5 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
-                >
-                  <div className="flex items-center">
-                    <a href={url} className="w-6 h-6 md:w-8 md:h-8 ">
-                      <span className="text-gray-600 h-6 w-6 ">{icon}</span>
-                      <span className="ml-2 xl:text-base md:text-2xl text-base">
-                        {title}
-                      </span>
-                    </a>
-                  </div>
-                </li>
-              ))}
-            </ul>
           </nav>
           {/* Sidebar ends */}
 
