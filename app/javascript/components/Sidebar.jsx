a9f6b37d41cc5937de1a0891d83b1a96ffc73649
Addisu <addisuhaile87@gmail.com>
2023-02-16 23:47:28 +0300

    Update side and navigation bars position

--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -26,41 +26,39 @@ const Sidebar = () => {
       <div className="w-full h-full bg-gray-200">
         <div className="flex flex-no-wrap">
           {/* Sidebar starts */}
-          <nav className="absolute h-screen z-40 lg:relative w-64 md:w-96 drop-shadow-md pb-4 bg-gray-100 sm:hidden lg:block transition duration-150 ease-in-out">
-            <div className="bg-gray-200 opacity-50 absolute h-full w-full">
-              <button
-                type="button"
-                className="absolute z-40 cursor-pointer border-none left-8 right-8"
-                onClick={() => setShow(!show)}
-              >
-                {show ? (
-                  <MdClose className="text-color-gray w-10 h-10" />
-                ) : (
-                  <FaBars className="text-color-gray w-10 h-10" />
-                )}
-              </button>
+          <nav className="w-48 absolute h-screen z-40 md:w-64 drop-shadow-md pb-4 bg-gray-200 sm:hidden lg:block transition duration-150 ease-in-out opacity-50">
+            <button
+              type="button"
+              className="absolute z-40 cursor-pointer border-none left-8 right-8"
+              onClick={() => setShow(!show)}
+            >
+              {show ? (
+                <MdClose className="text-color-gray w-10 h-10" />
+              ) : (
+                <FaBars className="text-color-gray w-10 h-10" />
+              )}
+            </button>
 
-              <ul
-                aria-orientation="vertical"
-                className={`py-6 ${show ? 'w-full' : 'hidden'}`}
-              >
-                {SideLinks.map(({ title, url, icon }, i) => (
-                  <li
-                    key={i}
-                    className="pl-6 cursor-pointer text-slate-700 text-sm leading-3 tracking-normal pb-4 pt-5 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
-                  >
-                    <div className="flex items-center">
-                      <a href={url} className="w-6 h-6 md:w-8 md:h-8 ">
-                        <span className="text-gray-600 h-6 w-6 ">{icon}</span>
-                        <span className="ml-2 xl:text-base md:text-2xl text-base">
-                          {title}
-                        </span>
-                      </a>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <ul
+              aria-orientation="vertical"
+              className={`py-6 ${show ? 'w-full' : 'hidden'}`}
+            >
+              {SideLinks.map(({ title, url, icon }, i) => (
+                <li
+                  key={i}
+                  className="pl-6 cursor-pointer text-slate-700 text-sm leading-3 tracking-normal pb-4 pt-5 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
+                >
+                  <div className="flex items-center">
+                    <a href={url} className="w-6 h-6 md:w-8 md:h-8 ">
+                      <span className="text-gray-600 h-6 w-6 ">{icon}</span>
+                      <span className="ml-2 xl:text-base md:text-2xl text-base">
+                        {title}
+                      </span>
+                    </a>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </nav>
           {/* Sidebar ends */}
 
@@ -73,7 +71,6 @@ const Sidebar = () => {
                     <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
                       <FaSearch className="w-4 h-4" />
                     </div>
-
                     <input
                       className="border border-gray-100 focus:outline-none focus:border-indigo-700 rounded w-full text-sm text-gray-500 bg-gray-100 pl-12 py-2"
                       type="text"
@@ -94,13 +91,6 @@ const Sidebar = () => {
               </div>
             </nav>
             {/* Navigation ends */}
-            <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-              <div className="w-full h-full ">
-                {Pages.map(({ page, url }, i) => (
-                  <a href={url}>{page}</a>
-                ))}
-              </div>
-            </div>
           </div>
         </div>
       </div>
