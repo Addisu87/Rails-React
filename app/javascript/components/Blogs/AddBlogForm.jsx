180b94bda5e5305f83921331ba201ad85e38236a
Addisu <addisuhaile87@gmail.com>
2023-02-17 17:18:20 +0300

    Update blog form

--- app/javascript/components/Blogs/AddBlogForm.jsx
+++ app/javascript/components/Blogs/AddBlogForm.jsx
@@ -44,79 +44,77 @@ const AddBlogForm = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <section className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-      <div className="flex relative flex-col md:text-left md:flex-row  max-w-7xl px-10 justify-evenly mx-auto items-center">
-        <div className="mt-20 mb-12 md:col-span-2 md:mt-24 space-y-8">
-          <div className="px-4 sm:px-0">
-            <h3 className="text-xl text-center font-medium leading-6 text-gray-900">
-              Add a New Blog
-            </h3>
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="overflow-hidden drop-shadow-2xl sm:rounded-md">
-              <div className="bg-white px-4 py-5 sm:p-6">
-                <div className="grid grid-cols-6 gap-6">
-                  <div className="col-span-6">
-                    <label htmlFor="blogTitle">Blog Title:</label>
-                    <input
-                      type="blogTitle"
-                      name="blogTitle"
-                      {...register('blogTitle', {
-                        required: true,
-                        maxLength: 20
-                      })}
-                      placeholder="blogTitle"
-                      id="blogTitle"
-                      autoComplete="blogTitle"
-                      value={title}
-                      onChange={onTitleChanged}
-                      className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    />
-                  </div>
+    <section className="container relative flex-col md:text-left md:flex-row  max-w-3xl px-10 justify-evenly mx-auto items-center">
+      <div className="mb-12 md:col-span-2 space-y-8">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-xl text-center font-medium leading-6 text-gray-900">
+            Add a New Blog
+          </h3>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="overflow-hidden drop-shadow-2xl sm:rounded-md">
+            <div className="bg-white px-4 py-5 sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6">
+                  <label htmlFor="blogTitle">Blog Title:</label>
+                  <input
+                    type="blogTitle"
+                    name="blogTitle"
+                    {...register('blogTitle', {
+                      required: true,
+                      maxLength: 20
+                    })}
+                    placeholder="blogTitle"
+                    id="blogTitle"
+                    autoComplete="blogTitle"
+                    value={title}
+                    onChange={onTitleChanged}
+                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
 
-                  <div className="col-span-6">
-                    <label htmlFor="blogAuthor">Author:</label>
-                    <select
-                      id="blogAuthor"
-                      value={userId}
-                      onChange={onAuthorChanged}
-                      {...register('blogAuthor')}
-                      className="mt-1 block w-full border-gray-300 shadow-sm
+                <div className="col-span-6">
+                  <label htmlFor="blogAuthor">Author:</label>
+                  <select
+                    id="blogAuthor"
+                    value={userId}
+                    onChange={onAuthorChanged}
+                    {...register('blogAuthor')}
+                    className="mt-1 block w-full border-gray-300 shadow-sm
                       focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    >
-                      <option value=""></option>
-                      {usersOptions}
-                    </select>
-                  </div>
+                  >
+                    <option value=""></option>
+                    {usersOptions}
+                  </select>
+                </div>
 
-                  <div className="col-span-6">
-                    <label htmlFor="blogContent">Content:</label>
-                    <textarea
-                      id="blogContent"
-                      name="blogContent"
-                      value={content}
-                      onChange={onContentChanged}
-                      {...register('blogContent', { required: true })}
-                      placeholder="blogContent"
-                      rows={4}
-                      className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    />
-                  </div>
+                <div className="col-span-6">
+                  <label htmlFor="blogContent">Content:</label>
+                  <textarea
+                    id="blogContent"
+                    name="blogContent"
+                    value={content}
+                    onChange={onContentChanged}
+                    {...register('blogContent', { required: true })}
+                    placeholder="blogContent"
+                    rows={4}
+                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 text-center md:text-left sm:px-6">
-                <button
-                  type="submit"
-                  onClick={onSaveBlogClicked}
-                  disabled={!canSave}
-                  className="inline-flex justify-center rounded-2xl border border-transparent bg-zinc-900 px-16 py-3 md:py-2 md:px-8 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2"
-                >
-                  Save blog
-                </button>
-              </div>
             </div>
-          </form>
-        </div>
+            <div className="bg-gray-50 px-4 py-3 text-center md:text-left sm:px-6">
+              <button
+                type="submit"
+                onClick={onSaveBlogClicked}
+                disabled={!canSave}
+                className="inline-flex justify-center rounded-2xl border border-transparent bg-zinc-900 px-16 py-3 md:py-2 md:px-8 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2"
+              >
+                Save blog
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </section>
   );
