c15c11f4823152fd6c8d85f50fba5f317b764290
Addisu <addisuhaile87@gmail.com>
2023-02-17 10:16:45 +0300

    Modify the side bar contents

--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  FaArrowCircleRight,
   FaBars,
   FaBell,
   FaDesktop,
@@ -28,30 +29,95 @@ const Sidebar = () => {
           <button className="flex justify-between items-center span-x-3">
             <FaDesktop />
           </button>
-          {/* Sidebar starts */}
-          <nav className="w-48 absolute h-screen z-40 md:w-64 drop-shadow-md pb-4 bg-gray-200 sm:hidden lg:block transition duration-150 ease-in-out opacity-50">
-            <ul
-              aria-orientation="vertical"
-              className={`py-6 ${show ? 'w-full' : 'hidden'}`}
+          <div
+            aria-label="toggler"
+            className="flex justify-center items-center"
+          >
+            <button
+              id="open"
+              onClick={() => setShow(!show)}
+              aria-label="open"
+              className={`${
+                show ? '' : 'hidden'
+              } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800`}
             >
-              {SideLinks.map(({ title, url, icon }, i) => (
-                <li
-                  key={i}
-                  className="pl-6 cursor-pointer text-slate-700 text-sm leading-3 tracking-normal pb-4 pt-5 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
-                >
-                  <div className="flex items-center">
-                    <a href={url} className="w-6 h-6 md:w-8 md:h-8 ">
-                      <span className="text-gray-600 h-6 w-6 ">{icon}</span>
-                      <span className="ml-2 xl:text-base md:text-2xl text-base">
-                        {title}
-                      </span>
-                    </a>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </nav>
+              <FaBars className="text-gray-800 h-8 w-8" />
+            </button>
+            <button
+              id="close"
+              onClick={() => setShow(!show)}
+              aria-label="close"
+              className={`${
+                show ? 'hidden' : ''
+              } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800`}
+            >
+              <MdClose className="text-gray-800 h-8 w-8" />
+            </button>
+          </div>
         </div>
+        {/* Sidebar starts */}
+        <nav
+          id="main"
+          className={`${
+            show ? '-translate-x-full' : 'translate-x-0'
+          }bg-white transform xl:translate-x-0 ease-in-out transition duration-500 flex flex-col justify-start items-start sm:w-72 w-full h-full`}
+        >
+          <button className="hidden xl:flex text-gray-800 hover:text-black focus:outline-none focus:text-black justify-start px-6 pt-6 items-center space-x-3 w-full">
+            <FaDesktop />
+            <p className="text-gray-900">Side Bar</p>
+          </button>
+          <div className="relative focus:outline-none flex justify-start w-full text-gray-800 rounded items-center border border-gray-300 focus:border-gray-400">
+            <input
+              type="text"
+              placeholder="Search"
+              className="placeholder-gray-800 text-base placeholder-text-base leading-4 py-3 px-8 w-full pl-16 rounded focus:outline-none focus:ring-offset-2 focus:ring-gray-600"
+            />
+            <FaSearch className="absolute left-4 w-5 h-5" />
+          </div>
+
+          <ul className="xl:mt-6 flex flex-col justify-start items-center px-4 w-full space-y-3 pb-5">
+            {SideLinks.map(({ title, url, icon }, i) => (
+              <li
+                key={i}
+                className="focus:outline-none flex justify-start hover:text-white focus:bg-indigo-700 focus:text-white hover:bg-indigo-700 text-gray-600 rounded py-3 pl-4 items-center space-x-6 w-full"
+              >
+                <a href={url}>
+                  <span className="">{icon}</span>
+                  <span className="text-base leading-4">{title}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="w-full px-4">
+            <hr className=" border-gray-100 w-full" />
+          </div>
+          <div
+            className="mt-36 flex bg-indigo-700 justify-start space-x-2
+            items-center h-full py-4 px-3.5 w-full "
+          >
+            <div>
+              <img
+                src="https://e7.pngegg.com/pngimages/211/502/png-clipart-lionel-messi-art-lionel-messi-wpap-desktop-lionel-messi-face-poster.png"
+                alt="avatar"
+                className="rounded-full w-10 h-10"
+              />
+            </div>
+            <div className="flex flex-col justify-start items-start space-y-2">
+              <p className="cursor-pointer text-base leading-4 text-white">
+                Addisu Haile
+              </p>
+              <p className="cursor-pointer text-xs leading-3 text-gray-200">
+                addisu.haile@yahoo.com
+              </p>
+            </div>
+            <button
+              aria-label="visit"
+              className=" focus:ring-2 focus:outline-none hover:bg-indigo-600 p-2.5 bg-indigo-400 rounded-full"
+            >
+              <FaArrowCircleRight />
+            </button>
+          </div>
+        </nav>
       </div>
     </>
   );
