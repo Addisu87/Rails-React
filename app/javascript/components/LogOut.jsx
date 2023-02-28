38a3a9974815f352a3c01fb739a1a51881e10fe8
Addisu <addisuhaile87@gmail.com>
2023-02-28 17:20:19 +0300

    Update logout page

--- app/javascript/components/LogOut.jsx
+++ app/javascript/components/LogOut.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import client from '../redux/axios';
 
-const LogOut = () => {
+const LogOut = (setCurrUser) => {
+  const logout = async () => {
+    localStorage.removeItem('token');
+    setCurrUser(null);
+    return await client
+      .delete('/logout')
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    logout(setCurrUser);
+  };
+
   return (
     <div>
-      <input type="button" value="logout" />
+      <input type="button" value="logout" onClick={handleClick} />
     </div>
   );
 };
