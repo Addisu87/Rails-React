5e9bef9cc75e9c05426a6e54ed86088e7bef90c2
Addisu <addisuhaile87@gmail.com>
2023-03-02 18:31:26 +0300

    Delete the link for navbar

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -59,11 +59,7 @@ const Navbar = () => {
 
         <ul className={`${openNav ? 'flex flex-col' : 'hidden md:flex'}`}>
           {navigation.map((item, index) => (
-            <li
-              key={index}
-              className="ml-10 flex items-baseline"
-              onClick={() => setOpenNav(!openNav)}
-            >
+            <li key={index} className="ml-10 flex items-baseline">
               <NavLink
                 to={item.url}
                 className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
